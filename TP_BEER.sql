107f2e7d8d6c8ecc63db73d2af89cec6cdbc79f1
Valentin Fadloun <v.fadloun@gmail.com>
2020-03-19 09:32:37 +0100

    Question 4

--- TP_BEER.sql
+++ TP_BEER.sql
@@ -5,3 +5,5 @@ SELECT ventes.NUMERO_TICKET FROM ventes INNER JOIN article ON ventes.ID_ARTICLE 
 SELECT DISTINCT ticket.* FROM ventes INNER JOIN ticket ON ventes.NUMERO_TICKET = ticket.NUMERO_TICKET AND ventes.ANNEE = ticket.ANNEE WHERE ticket.DATE_VENTE = "2014-01-15 00:00:00";
 
 SELECT DISTINCT ticket.* FROM ventes INNER JOIN ticket ON ventes.NUMERO_TICKET = ticket.NUMERO_TICKET AND ventes.ANNEE = ticket.ANNEE WHERE ticket.DATE_VENTE BETWEEN "2014-01-15 00:00:00" and "2014-01-17 00:00:00";
+
+SELECT article.* FROM article INNER JOIN ventes ON ventes.ID_ARTICLE = article.ID_ARTICLE WHERE ventes.QUANTITE >= 50 ;
