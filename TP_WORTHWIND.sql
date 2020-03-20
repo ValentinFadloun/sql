6db00e2b8cac066e6e669b80fc7cb2a1f549766e
Valentin Fadloun <v.fadloun@gmail.com>
2020-03-20 16:11:18 +0100

    1 northwind - step 2

--- TP_WORTHWIND.sql
+++ TP_WORTHWIND.sql
@@ -1,2 +1,4 @@
 # 1
 SELECT * FROM employees WHERE city = "Seattle";
+SELECT * FROM customers WHERE city = "Seattle";
+SELECT DISTINCT (SELECT COUNT(*) FROM employees WHERE city = "Seattle")+(SELECT COUNT(*) FROM customers WHERE city = "Seattle") as personnesSeatles FROM (SELECT * FROM employees WHERE city = "Seattle") as employees;
