f2d094c7ecebc7aec986a527378a0fc8a34654ae
Valentin Fadloun <v.fadloun@gmail.com>
2020-03-20 16:30:03 +0100

    1 northwind - step 3

--- TP_WORTHWIND.sql
+++ TP_WORTHWIND.sql
@@ -1,4 +1,9 @@
 # 1
 SELECT * FROM employees WHERE city = "Seattle";
 SELECT * FROM customers WHERE city = "Seattle";
-SELECT DISTINCT (SELECT COUNT(*) FROM employees WHERE city = "Seattle")+(SELECT COUNT(*) FROM customers WHERE city = "Seattle") as personnesSeatles FROM (SELECT * FROM employees WHERE city = "Seattle") as employees;
+SELECT DISTINCT CONCAT(employees.last_name, " ",employees.first_name),
+				(SELECT COUNT(*) FROM employees WHERE city = "Seattle")+(SELECT COUNT(*) FROM customers WHERE city = "Seattle") as 'nombre de personnes de Seatles'
+FROM (SELECT * FROM employees WHERE city = "Seattle") as employees;
+
+SELECT (SELECT COUNT(*) FROM employees WHERE city = "Seattle")+(SELECT COUNT(*) FROM customers WHERE city = "Seattle") as 'nombre de personnes de Seatles', last_name, first_name 
+FROM (SELECT * FROM employees WHERE city = "Seattle" UNION SELECT * FROM customers WHERE city = "Seattle") as employees;
